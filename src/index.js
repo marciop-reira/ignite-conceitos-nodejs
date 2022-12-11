20e59d66921e172627d5d9fd8e86ebb8bcf9c469
Márcio Pereira <marciop.usa@gmail.com>
2022-12-11 15:53:37 -0300

    Complete CRUD todo

--- src/index.js
+++ src/index.js
@@ -28,6 +28,13 @@ function checksExistsUserAccount(request, response, next) {
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
+  const userAlreadyExists = users.some(user => user.username === username);
+  if (userAlreadyExists) {
+    return response.status(400).json({
+      error: 'User already exists'
+    });
+  }
+
   const user = {
     id: uuidv4(),
     name,
@@ -47,19 +54,57 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  todo.done = true;
+
+  return response.status(200).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (todo) {
+    user.todos.splice(user.todos.indexOf(todo), 1);
+  }
+
+  return response.status(200).json(user.todos);
 });
 
 module.exports = app;
